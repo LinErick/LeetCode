b069aab94bdb55e1f47f575b9d6727748a451425
LinErick <50795512+LinErick@users.noreply.github.com>
2021-11-23 13:40:47 +0800

    Implement strStr()
    Add Practice

---
+++ ImplementStrsStr/ImplementStrsStr/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ImplementStrsStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StrStr("mississippi", "issip");
+            StrStr("hello", "ll");
+            StrStr("aaa", "aaaa");
+        }
+        public static int StrStr(string haystack, string needle)
+        {
+            if (needle == string.Empty)
+                return 0;
+
+            int success = 0;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[success])
+                {
+                    success++;
+
+                    if (needle.Length == success)
+                        return i - success + 1;
+                }
+                else
+                {
+                    i -= success;
+                    success = 0;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
